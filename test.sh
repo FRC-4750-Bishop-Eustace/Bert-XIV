74e5ac090b92e902e82efefa4dad22f56b53bf5d
Rohan Bharatia <Rohan-Bharatia@github.com>
2025-12-27 19:35:51 -0500

    Fix  file

--- test.sh
+++ test.sh
@@ -2,47 +2,17 @@
 
 set -e
 
-case "$(uname -s)" in
-    Darwin)
-        if [ ! -x "$(command -v brew)" ]; then
-            /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        fi
-        ;;
-    Linux)
-        ;;
-    *)
-        echo "Unsupported operating system."
-        exit 1
-esac
+SKIP_SYNC=false
+for arg in "$@"; do
+    if [ "$arg" = "--skip-sync" ]; then
+        SKIP_SYNC=true
+    fi
+done
 
-if [ ! -x "$(command -v docker)" ]; then
-    case "$(uname -s)" in
-        Darwin)
-            brew install docker
-            ;;
-        Linux)
-            mkdir -p bin
-            curl -fsSL https://get.docker.com -o bin/get-docker.sh
-            sudo sh bin/get-docker.sh
-            ;;
-        *)
-            echo "Docker is not installed. Please install it."
-            exit 1
-    esac
+if [ "$SKIP_SYNC" = false ]; then
+    python3 -m robotpy sync
 fi
+python3 -m robotpy create-physics
+python3 -m robotpy test --builtin
 
-if [ ! -x "$(command -v act)" ]; then
-    case "$(uname -s)" in
-        Darwin)
-            brew install act
-            ;;
-        Linux)
-            curl -sSL https://raw.githubusercontent.com/nektos/act/master/install.sh | sh
-            ;;
-        *)
-            echo "act is not installed. Please install it."
-            exit 1
-    esac
-fi
-
-sudo ./bin/act push -P ubuntu-latest=ghcr.io/catthehacker/ubuntu:full-latest
+rm physics.py
