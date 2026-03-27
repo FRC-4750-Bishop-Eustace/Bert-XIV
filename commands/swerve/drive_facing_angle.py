55ad5b7506755053434ca65653560b13f3365b6a
Noah Nguyen <nnoah4941@gmail.com>
2026-03-27 19:38:07 -0400

    Add missing command file

---
+++ commands/swerve/drive_facing_angle.py
@@ -0,0 +1,84 @@
+# MIT License
+#
+# Copyright (c) 2025 Beʳᵗ FRC Team 4750
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import constants
+from subsystems import *
+from wpilib import DriverStation, SmartDashboard
+from wpimath.controller import PIDController
+from commands2 import Command
+from wpilib import PS4Controller
+from wpimath import applyDeadband
+from wpimath.filter import SlewRateLimiter
+
+class DriveFacingAngle(Command):
+    def __init__(self, swerve: Drivetrain, controller: PS4Controller) -> None:
+        super().__init__()
+        self.swerve = swerve
+        self.controller = controller
+        self.xSlewRate = SlewRateLimiter(constants.xSlewRate)
+        self.ySlewRate = SlewRateLimiter(constants.ySlewRate)
+        self.fieldRelative = True
+        self.headingPID = constants.swerveHeadingPID.toPIDController()
+        self.headingPID.enableContinuousInput(-180, 180)
+        self.headingTarget = (self.swerve.gyro.GetYaw())
+
+        self.addRequirements(self.swerve)
+
+        SmartDashboard.putBoolean("Field Relative", self.fieldRelative)
+        SmartDashboard.putData("Heading PID Controller", self.headingPID)
+
+    def initialize(self) -> None:
+        self.headingTarget = (self.swerve.gyro.GetYaw())
+
+    def execute(self) -> None:
+        if not DriverStation.isTeleopEnabled():
+            return
+
+        xSpeed = -self.xSlewRate.calculate(
+            applyDeadband(
+                self.controller.getRawAxis(constants.ps4LeftY),
+                constants.xDeadband
+            ) * constants.maxSpeed
+        )
+        ySpeed = -self.ySlewRate.calculate(
+            applyDeadband(
+                self.controller.getRawAxis(constants.ps4LeftX),
+                constants.yDeadband
+            ) * constants.maxSpeed
+        )
+        rotSpeed = self.headingPID.calculate(self.swerve.gyro.GetYaw(), self.headingTarget)
+
+        if self.controller.getPOV() == constants.ps4Up:
+            xSpeed = 0.2
+        if self.controller.getPOV() == constants.ps4Right:
+            ySpeed = -0.2
+        if self.controller.getPOV() == constants.ps4Down:
+            xSpeed = -0.2
+        if self.controller.getPOV() == constants.ps4Left:
+            ySpeed = 0.2
+
+        if self.controller.getRawButton(constants.ps4L1) == 1:
+            rotSpeed = 0.5
+        if self.controller.getRawButton(constants.ps4R1) == 1:
+            rotSpeed = -0.5
+
+        self.swerve.drive(xSpeed, ySpeed, rotSpeed, self.fieldRelative, 0.02)
